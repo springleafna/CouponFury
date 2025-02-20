4bf5474f1d95db3770d479ee1e5104ec17c8be41
springleaf <3313252209@qq.com>
2025-02-20 08:33:12 +0800

    feat:通过配置开关enabled决定是否使用XXL-Job

--- CouponFury-merchant-admin/src/main/java/com/springleaf/couponfury/merchant/admin/config/XXLJobConfiguration.java
+++ CouponFury-merchant-admin/src/main/java/com/springleaf/couponfury/merchant/admin/config/XXLJobConfiguration.java
@@ -13,6 +13,13 @@ import java.nio.file.Paths;
  * XXL-Job 配置类
  */
 @Configuration
+// @ConditionalOnProperty主要用于在特定条件下启用或禁用 Spring Bean 或配置。
+// 通过该注解，可以实现基于配置属性的条件化 Bean 装配
+/*
+prefix：表示要检查的属性的前缀为 xxl-job。
+name：指定要检查的属性名称是 enabled，结合前缀，完整的属性名称为 xxl-job.enabled。
+havingValue：指定属性 xxl-job.enabled 的值应该为 "true"，当且仅当属性值匹配 "true" 时，相关的 Bean 或配置才会被加载。
+matchIfMissing：如果未找到属性 xxl-job.enabled，将默认视为匹配。*/
 @ConditionalOnProperty(prefix = "xxl-job", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class XXLJobConfiguration {
 
