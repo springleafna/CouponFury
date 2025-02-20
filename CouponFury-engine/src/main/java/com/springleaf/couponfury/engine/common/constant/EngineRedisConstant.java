a1d0fff8dd02d54b87717f7bad1a374824030a85
springleaf <3313252209@qq.com>
2025-02-20 18:52:01 +0800

    feat:改造优惠券模板查询缓存穿透

--- CouponFury-engine/src/main/java/com/springleaf/couponfury/engine/common/constant/EngineRedisConstant.java
+++ CouponFury-engine/src/main/java/com/springleaf/couponfury/engine/common/constant/EngineRedisConstant.java
@@ -14,4 +14,9 @@ public final class EngineRedisConstant {
      * 优惠券模板缓存分布式锁 Key
      */
     public static final String LOCK_COUPON_TEMPLATE_KEY = "coupon-fury_engine:lock:template:%s";
+
+    /**
+     * 优惠券模板缓存空值 Key
+     */
+    public static final String COUPON_TEMPLATE_IS_NULL_KEY = "coupon-fury_engine:template_is_null:%s";
 }
--- CouponFury-engine/src/main/java/com/springleaf/couponfury/engine/service/impl/CouponTemplateServiceImpl.java
+++ CouponFury-engine/src/main/java/com/springleaf/couponfury/engine/service/impl/CouponTemplateServiceImpl.java
@@ -12,6 +12,7 @@ import com.springleaf.couponfury.engine.dto.resp.CouponTemplateQueryRespDTO;
 import com.springleaf.couponfury.engine.service.CouponTemplateService;
 import com.springleaf.couponfury.framework.exception.ClientException;
 import jakarta.annotation.Resource;
+import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -22,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -36,6 +38,8 @@ public class CouponTemplateServiceImpl implements CouponTemplateService {
     private StringRedisTemplate stringRedisTemplate;
     @Resource
     private RedissonClient redissonClient;
+    @Resource
+    private RBloomFilter<String> couponTemplateQueryBloomFilter;
 
     @Override
     public CouponTemplateQueryRespDTO findCouponTemplate(CouponTemplateQueryReqDTO requestParam) {
@@ -43,20 +47,39 @@ public class CouponTemplateServiceImpl implements CouponTemplateService {
         String couponTemplateCacheKey = String.format(EngineRedisConstant.COUPON_TEMPLATE_KEY, requestParam.getCouponTemplateId());
         Map<Object, Object> couponTemplateCacheMap = stringRedisTemplate.opsForHash().entries(couponTemplateCacheKey);
 
-        // 如果存在直接返回，不存在需要通过双重判定锁的形式读取数据库中的记录
+        // 如果存在直接返回，不存在需要通过布隆过滤器、缓存空值以及双重判定锁的形式读取数据库中的记录
         if (MapUtil.isEmpty(couponTemplateCacheMap)) {
+            // 判断布隆过滤器是否存在指定模板ID，不存在直接返回错误
+            if (!couponTemplateQueryBloomFilter.contains(String.valueOf(requestParam.getCouponTemplateId()))) {
+                throw new ClientException("优惠券模板不存在或已过期");
+            }
+
+            // 查询 Redis 缓存中是否存在优惠券模板空值信息，如果有代表模板不存在，直接返回
+            String couponTemplateIsNullCacheKey = String.format(EngineRedisConstant.COUPON_TEMPLATE_IS_NULL_KEY, requestParam.getCouponTemplateId());
+            Boolean hasKeyFlag = stringRedisTemplate.hasKey(couponTemplateIsNullCacheKey);
+            if (Boolean.TRUE.equals(hasKeyFlag)) {
+                throw new ClientException("优惠券模板不存在或已过期");
+            }
+
             // 获取优惠券模板分布式锁
             RLock lock = redissonClient.getLock(String.format(EngineRedisConstant.LOCK_COUPON_TEMPLATE_KEY, requestParam.getCouponTemplateId()));
             lock.lock();
 
             try {
+                // 双重判定空值缓存是否存在，存在则继续抛异常
+                hasKeyFlag = stringRedisTemplate.hasKey(couponTemplateIsNullCacheKey);
+                if (Boolean.TRUE.equals(hasKeyFlag)) {
+                    throw new ClientException("优惠券模板不存在或已过期");
+                }
+
                 // 通过双重判定锁优化大量请求无意义查询数据库
                 couponTemplateCacheMap = stringRedisTemplate.opsForHash().entries(couponTemplateCacheKey);
                 if (MapUtil.isEmpty(couponTemplateCacheMap)) {
                     CouponTemplateDO couponTemplateDO = couponTemplateMapper.getCouponTemplateByShopNumberAndId(UserContext.getShopNumber(), requestParam.getCouponTemplateId());
 
-                    // 优惠券模板不存在或者已过期直接抛出异常
+                    // 优惠券模板不存在或者已过期加入空值缓存，并且抛出异常
                     if (couponTemplateDO == null) {
+                        stringRedisTemplate.opsForValue().set(couponTemplateIsNullCacheKey, "", 30, TimeUnit.MINUTES);
                         throw new ClientException("优惠券模板不存在或已过期");
                     }
 
